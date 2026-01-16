88afe25032305f2be3ed0ddd627da38d4637a4e1
angelorapana <angelorapana@icloud.com>
2026-01-16 16:07:33 +0100

    esecuzione snack 6 con metodo foreach (necessaria verifica)

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,16 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+let zucchineLunghe = []
+let zucchineRimanenti = []
+
+zucchine.forEach((ricercaZucchine)=> {  // Ho una variabile locale e devo solo scorrere l'array quindi utilizzo foreach
+    if (ricercaZucchine >= 15){
+      zucchineLungheLunghe.push(ricercaZucchine)}
+      else {
+        zucchineRimanenti.push(ricercaZucchine)}
+  });
+
+console.log(zucchineLunghe)
+console.log(zucchineRimanenti)
