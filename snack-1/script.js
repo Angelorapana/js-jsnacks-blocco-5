50a8f2f05ac71214d369dc9231a52376db9b7db1
angelorapana <angelorapana@icloud.com>
2026-01-16 15:01:18 +0100

    esecuzione snack uno con metodo array forEach

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,10 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+//console.log(names); //cosi stampo l'array
+//Utilizzando foreach cerco i nomi nell'array e li stampo
+
+names.forEach((cercaNome)=>{
+    console.log(cercaNome)
+})
