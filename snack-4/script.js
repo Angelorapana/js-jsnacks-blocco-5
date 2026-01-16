ade292926f641c0f437e5bf301656c0afda0c9ac
angelorapana <angelorapana@icloud.com>
2026-01-16 15:23:45 +0100

    esecuzione snack 4 utilizzando map

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+posts.map((nomiautori) => {
+  let {author} = posts
+  let gruppoNomi = nomiautori.author
+  console.log(gruppoNomi);
+});
